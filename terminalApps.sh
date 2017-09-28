b61ec059c88a460fa14d28781cde4bbc8c49e0f3
Parth Krishna <parthkrishna1611@gmail.com>
2017-09-28 14:08:44 +0530

    added cli apps

---
+++ terminalApps.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+##########################################                                      
+#
+# MMMMMMMMMMMMMMMMMMMMMMMMMmds+.        
+# MMm----::-://////////////oymNMd+`     
+# MMd      /++                -sNMd:    
+# MMNso/`  dMM    `.::-. .-::.` .hMN:   
+# ddddMMh  dMM   :hNMNMNhNMNMNh: `NMm   
+#     NMm  dMM  .NMN/-+MMM+-/NMN` dMM   
+#     NMm  dMM  -MMm  `MMM   dMM. dMM   
+#     NMm  dMM  -MMm  `MMM   dMM. dMM   
+#     NMm  dMM  .mmd  `mmm   yMM. dMM   
+#     NMm  dMM`  ..`   ...   ydm. dMM   
+#     hMM- +MMd/-------...-:sdds  dMM   
+#     -NMm- :hNMNNNmdddddddddy/`  dMM   
+#      -dMNs-``-::::-------.``    dMM   
+#       `/dMNmy+/:-------------:/yMMM  
+#          ./ydNMMMMMMMMMMMMMMMMMMMMM  
+#             \.MMMMMMMMMMMMMMMMMMM    
+#                                      
+##########################################
+echo "###############################"
+
+echo "Hello"
+echo "This will install cli apps"
+echo "###############################"
+
+# vim
+sudo apt-get install -y vim
+
+# To autocorrect your terminal commands
+pip install thefuck
+
+# change wallpapers with feh
+sudo apt-get install -y feh
+
+# take picture of screen
+sudo apt-get install -y scrot
+
+# ranger - file manager
+sudo apt-get install -y ranger
+
+# mplayer - media player
+sudo apt-get install -y mplayer
+
+# list all installed apps
+# ls /usr/share/applications | awk -F '.desktop' ' { print $1}' - > ~/Desktop/applications.txt
